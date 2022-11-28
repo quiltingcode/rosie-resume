eba3bcd8a19a04e158cac5dfc17ec24bbbbb669f
Kelly Hutchison <kellyhutchison@hotmail.com>
2022-11-28 14:41:45 +0000

    Add functions to send projects request to my email

---
+++ assets/js/sendEmail.js
@@ -0,0 +1,15 @@
+function sendMail(contactForm) {
+    emailjs.sendForm("service_kcg0vxp", "resume-rosie", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "project_request": contactForm.projectsummary.value
+    })
+    .then(
+        function(response) {
+            console.log("SuCcess", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        });
+}
+
