075a9f6a69a162416c10884091548e6f02e547e2
Kelly Hutchison <kellyhutchison@hotmail.com>
2022-11-28 10:59:41 +0000

    Add error message when API limit is reached

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -62,10 +62,13 @@ function fetchGitHubInformation(event) {
             $("#gh-user-data").html(userInformationHTML(userData));
             $("#gh-repo-data").html(repoInformationHTML(repoData));
         },
-        function(errorResponse) {
+        function(errorRsesponse) {
             if (errorResponse.status === 404) {
                 $("#gh-user-data").html(
                     `<h2>No info found for user ${username}</h2>`);
+            } else if(errorResponse.status === 403) {
+                var resetTime = new Date(errorResponse.getResponseHeader('x-RateLimit-Reset')*1000);
+                $("#gh-user-data").html(`<h4>Too many requests, please wait until ${resetTime.toLocaleTimeString()}</h4>`)
             } else {
                 console.log(errorResponse);
                 $("#gh-user-data").html(
